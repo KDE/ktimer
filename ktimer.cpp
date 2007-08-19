05d3f38fcc8542ee1ee78f5431d8993a60d32ef7
Arto Hytönen <amthpublic@gmail.com>
2007-08-19 10:51:37 +0000

    mostly html tags and other tidying up with few explicit ctor enforcements stuffed too, so basicly a EBN clean-up on kdeutils.
    svn path=/trunk/KDE/kdeutils/ktimer/; revision=701796

--- ktimer.cpp
+++ ktimer.cpp
@@ -1,5 +1,5 @@
 /*
- * (c) 2001 Stefan Schimanski <1Stein@gmx.de>
+ * Copyright 2001 Stefan Schimanski <1Stein@gmx.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * (c) 2001 Stefan Schimanski <schimmi@kde.org>
+ * Copyright 2001 Stefan Schimanski <schimmi@kde.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

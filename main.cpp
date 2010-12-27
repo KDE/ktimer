3be89fa1115e0190b628827a2dce60125c4c1350
Friedrich W. H. Kossebau <kossebau@kde.org>
2010-12-27 16:16:23 +0000

    Changes: bump version numbers
    CCMAIL: kde-utils-devel@kde.org
    
    svn path=/trunk/KDE/kdeutils/ktimer/; revision=1209623

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@
 static const char description[] =
         I18N_NOOP("KDE Timer");
 
-static const char version[] = "v0.7";
+static const char version[] = "v0.8";
 
 int main( int argc, char **argv )
 {

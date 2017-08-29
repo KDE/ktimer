a5ee141d7b387b031f82257ec6cd64e30c093f7f
Tomaz Canabrava <tcanabrava@kde.org>
2017-08-29 14:25:37 +0200

    Start the model that will hold the ktimer data

---
+++ src/ktimermodel.cpp
@@ -0,0 +1,35 @@
+#include "ktimermodel.h"
+
+KTimerModel::KTimerModel(QObject* parent) : QAbstractListModel(parent)
+{
+}
+
+QVariant KTimerModel::data(const QModelIndex& idx, int role) const
+{
+    if (! idx.isValid())
+        return {};
+
+    switch(role) {
+        case Qt::DisplayRole : return "Something";
+    }
+    return {};
+}
+
+bool KTimerModel::insertRows(int row, int count, const QModelIndex& parent)
+{
+    Q_UNUSED(row);
+    Q_UNUSED(count);
+    Q_UNUSED(parent);
+    return false;
+}
+
+int KTimerModel::rowCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return m_timerInfos.count();
+}
+
+QHash<int, QByteArray> KTimerModel::roleNames() const
+{
+    return {};
+}

84fb41dde61e5a9dc06c9157c091ce1bccc7115e
Tomaz Canabrava <tcanabrava@kde.org>
2017-08-29 15:05:10 +0200

    Q_PROPERTY for the ktimerjob
    Also, remove some uneeded methods and make everything int
    instead of unsigned. Nobody will need that much time
    for a timer.

--- src/ktimerjob.h
+++ src/ktimerjob.h
@@ -8,19 +8,28 @@
 class KConfig;
 class KTimerJob : public QObject {
  Q_OBJECT
+    Q_PROPERTY(int delay READ delay WRITE setDelay NOTIFY delayChanged)
+    Q_PROPERTY(QString command READ command WRITE setCommand NOTIFY commandChanged)
+    Q_PROPERTY(bool loop READ loop WRITE setLoop NOTIFY loopChanged)
+    Q_PROPERTY(bool oneInstance READ oneInstance WRITE setOneInstance NOTIFY oneInstanceChanged)
+    Q_PROPERTY(int value READ value WRITE setValue NOTIFY valueChanged)
+    Q_PROPERTY(States state READ state WRITE setState NOTIFY stateChanged)
 
- public:
+public:
     KTimerJob( QObject *parent=0);
     virtual ~KTimerJob();
 
     enum States { Stopped, Paused, Started };
+    Q_ENUM(States)
 
-    unsigned delay() const;
+    int delay() const;
     QString command() const;
     bool loop() const;
     bool oneInstance() const;
-    unsigned value() const;
+    int value() const;
     States state() const;
+
+    //TODO: Remove this void pointer.
     void *user();
     void setUser( void *user );
 
@@ -30,14 +39,11 @@ class KTimerJob : public QObject {
     int timeToSeconds( int hours, int minutes, int seconds ) const;
     void secondsToHMS( int secs, int *hours, int *minutes, int *seconds ) const;
 
-
  public slots:
-    void setDelay( unsigned int sec );
     void setDelay( int sec );
     void setCommand( const QString &cmd );
     void setLoop( bool loop );
     void setOneInstance( bool one );
-    void setValue( unsigned int value );
     void setValue( int value );
     void setState( States state );
 
@@ -47,7 +53,7 @@ class KTimerJob : public QObject {
 
  signals:
     void stateChanged(States state);
-    void delayChanged(unsigned int sec);
+    void delayChanged(int sec);
     void commandChanged(const QString &cmd);
     void loopChanged(bool loop);
     void oneInstanceChanged(bool one);

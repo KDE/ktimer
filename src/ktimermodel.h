13d3b8daec4f2c008437586995c7f87cd324734f
Tomaz Canabrava <tcanabrava@kde.org>
2017-08-29 14:25:37 +0200

    Start the model that will hold the ktimer data

---
+++ src/ktimermodel.h
@@ -0,0 +1,23 @@
+#ifndef KTIMERMODEL_H
+#define KTIMERMODEL_H
+
+#include <QAbstractListModel>
+#include <QList>
+
+class KTimerInfo;
+
+class KTimerModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    KTimerModel(QObject *parent = 0);
+    QVariant data(const QModelIndex& idx, int role) const override;
+    int rowCount(const QModelIndex& parent) const override;
+    bool insertRows(int row, int count, const QModelIndex& parent = QModelIndex()) override;
+    QHash<int, QByteArray> roleNames() const override;
+private:
+    QList<KTimerInfo*> m_timerInfos;
+
+};
+
+#endif
